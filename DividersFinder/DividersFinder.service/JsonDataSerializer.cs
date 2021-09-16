2e649fb61e6966906a5a1d4acef02ca54a9114a9
Mariusz Pudlik <marpudlik@wp.pl>
2021-09-16 17:46:35 +0200

    Ścieżka dostępu do json

--- DividersFinder/DividersFinder.service/JsonDataSerializer.cs
+++ DividersFinder/DividersFinder.service/JsonDataSerializer.cs
@@ -17,12 +17,12 @@ namespace DividersFinder.service
         public void Serialize(Dictionary<ulong, List<ulong>> resultOffinding)
         {
             var jsonData = JsonConvert.SerializeObject(resultOffinding, Formatting.Indented);
-            File.WriteAllText(@"C:\Users\user\source\repos\dividers_finder\DividersFinder\DividersFinder.service\Writelines5.json", jsonData);
+            File.WriteAllText(@"C:\Users\Mario\Source\Repos\dividers_finder\DividersFinder\DividersFinder.service\Writelines5.jso", jsonData);
         }
 
         public Dictionary<ulong,List<ulong>> Deserialize()
         {
-            var readjsonData = File.ReadAllText(@"C:\Users\user\source\repos\dividers_finder\DividersFinder\DividersFinder.service\Writelines5.json");
+            var readjsonData = File.ReadAllText(@"C:\Users\Mario\Source\Repos\dividers_finder\DividersFinder\DividersFinder.service\Writelines5.json");
             var readingData = JsonConvert.DeserializeObject<Dictionary<ulong, List<ulong>>>(readjsonData);
             return readingData;
         }
